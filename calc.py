0942b38024d6c06f4f03a61a7e5d4c1c72cdfce3
Supratik Panuganti <simbarps@gmail.com>
2026-08-18 17:52:43 -0700

    reset demo seed (bug planted)

--- calc.py
+++ calc.py
@@ -1,2 +1,2 @@
 def uptime_pct(up_s, total_s):
-    return round(100 * up_s / total_s, 2)
+    return round(100 * up_s // total_s, 2)
--- tests/test_calc.py
+++ tests/test_calc.py
@@ -2,3 +2,6 @@ from calc import uptime_pct
 
 def test_uptime_pct():
     assert uptime_pct(99, 100) == 99.0
+
+def test_uptime_pct_fractional():
+    assert uptime_pct(997, 1000) == 99.7
